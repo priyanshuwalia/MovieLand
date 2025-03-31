18da259f0d548591470178317c7331082b480ca3
Priyanshu Walia <waliapriyanshu07@gmail.com>
2025-03-31 22:05:53 +0530

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 import SearchIcon from "./search.svg";
 import MovieCard from "./movieCard";
 
-const API_URL = "http://www.omdbapi.com?apikey=b2d79b9f";
+const API_URL = "https://www.omdbapi.com?apikey=b2d79b9f";
 
 function App() {
   const [movies, setMovies] = useState([]);
